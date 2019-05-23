072a7504a4fa8e544bab59c43a75c5d5ee923c94
gzx <kang@51atm.com.cn>
2019-05-23 16:26:02 +0800

    spring-cloud学习，feign负载均衡

---
+++ spring-cloud-feign/service-feign/src/main/java/com/xiao/learn/service/feign/service/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package com.xiao.learn.service.feign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by guoxiaoxiao on 2019/5/23.
+ * Desc：
+ */
+@FeignClient(value = "eureka-client")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
